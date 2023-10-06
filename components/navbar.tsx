3fd21d616ad9db21514c65373dc7f1ea80f5a646
Joana Parente <jrparente@gmail.com>
2023-10-06 15:02:38 +0100

    Fixed padding in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -36,11 +36,11 @@ export default function Navbar() {
   return (
     <div
       className={cn(
-        "fixed w-full z-40 px-4 py-2 h-16 transition-all ease-in-out duration-300",
+        "fixed w-full z-40 py-2 h-16 transition-all ease-in-out duration-300",
         scrolled ? "bg-background/80 backdrop-blur-sm" : "bg-transparent"
       )}
     >
-      <div className="max-w-screen-xl mx-auto flex px-4 items-center justify-between ">
+      <div className="max-w-screen-xl mx-auto flex md:px-4 items-center justify-between ">
         <div className="flex items-center">
           <MobileSidebar />
           <Link href="/">

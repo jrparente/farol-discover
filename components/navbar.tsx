cd8e349242a64d0ac643dacec287eaddb5d855d0
Joana Parente <jrparente@gmail.com>
2024-07-31 14:23:50 +0100

    fix: Link in Navigation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -171,13 +171,13 @@ export default function Navbar() {
                                       <NavigationMenuLink asChild>
                                         <a
                                           className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md cursor-pointer"
-                                          href={subNavItem.linkUrl}
+                                          href={subNavItem.href}
                                         >
                                           <div className="mb-2 mt-4 text-lg font-medium">
                                             {subNavItem.title}
                                           </div>
                                           <p className="text-sm leading-tight text-muted-foreground">
-                                            {subNavItem.subtitle}
+                                            {subNavItem.description}
                                           </p>
                                         </a>
                                       </NavigationMenuLink>
